c60b3906c5cb6d5ead69b818fcb270851bfb0a62
kdev <kdev.software1110@gmail.com>
2018-12-24 22:04:34 -0500

    default ui structure configuration

---
+++ src/app/kdev-config/index.ts
@@ -0,0 +1,34 @@
+import { KdevConfig } from '@kdev/types';
+
+export const kdevConfig: KdevConfig = {
+    colorTheme: 'theme-default',
+    customScrollbars: true,
+    layout: {
+        style: 'admin',
+        width: 'fullwidth',
+        navbar: {
+            primaryBackground: 'kdev-navy-700',
+            secondaryBackground: 'kdev-navy-900',
+            folded: false,
+            hidden: false,
+            position: 'left',
+            variant: 'admin'
+        },
+        toolbar: {
+            customBackgroundColor: false,
+            background: 'kdev-white-500',
+            hidden: false,
+            position: 'below-static'
+        },
+        footer: {
+            customBackgroundColor: true,
+            background: 'kdev-navy-900',
+            hidden: false,
+            position: 'below-fixed'
+        },
+        sidepanel: {
+            hidden: false,
+            position: 'right'
+        }
+    }
+};
