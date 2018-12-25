5e234f0f4f31ff1311bd80e4d2db92e7451c7329
kdev <kdev.software1110@gmail.com>
2018-12-25 10:35:32 -0500

    define the routes for frontend pages

--- src/app/pages/frontend-pages/frontend-pages.module.ts
+++ src/app/pages/frontend-pages/frontend-pages.module.ts
@@ -1,10 +1,32 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
+import { KdevSharedModule } from '@kdev/shared.module';
+
+const routes = [
+  {
+    path: 'page-1',
+    loadChildren: './frontend-page1/frontend-page1.module#FrontendPage1Module'
+  },
+  {
+    path: 'page-2',
+    loadChildren: './frontend-page2/frontend-page2.module#FrontendPage2Module'
+  },
+  {
+    path: 'page-3',
+    loadChildren: './frontend-page3/frontend-page3.module#FrontendPage3Module'
+  },
+  {
+    path: 'login',
+    loadChildren: './login/login.module#LoginModule'
+  },
+];
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    RouterModule.forChild(routes),
+    KdevSharedModule
   ]
 })
 export class FrontendPagesModule { }
