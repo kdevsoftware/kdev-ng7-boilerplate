e35f393192f7134e06790fe65b6625b4d1faaf6e
kdev <kdev.software1110@gmail.com>
2018-12-24 21:57:34 -0500

    AppComponent from OnInit and OnDestroy

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'kdev-ng7-boilerplate';
+export class AppComponent implements OnInit, OnDestroy {
+  constructor() {
+
+  }
+
+  ngOnInit() {
+
+  }
+
+  ngOnDestroy() {
+    
+  }
 }
